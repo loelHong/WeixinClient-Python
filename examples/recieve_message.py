89d083d77c73b48da96d8477022dd6fdb0d9c5ee
haoyiqiang <>
2023-02-23 17:48:22 +0800

    fix bug

---
+++ examples/recieve_message.py
@@ -0,0 +1,11 @@
+from wechat import WechatHelper
+
+if __name__ == '__main__':
+    helper = WechatHelper("192.168.0.107")
+    helper.wait_until_login()
+
+    @helper.message("chat")
+    def recieve_message(topic, message):
+        print(message)
+
+    helper.run_forever()
