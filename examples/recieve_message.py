54469dca81a2b7eb440540167b90935c356ef2be
haoyiqiang <>
2023-02-23 20:11:33 +0800

    remove submodule

--- examples/recieve_message.py
+++ examples/recieve_message.py
@@ -2,10 +2,11 @@ from wechat import WechatHelper
 
 if __name__ == '__main__':
     helper = WechatHelper("192.168.0.107")
-    helper.wait_until_login()
+    self_info = helper.wait_until_login()
+    print(self_info)
 
     @helper.message("chat")
     def receive_message(topic, message):
         print(message)
 
-    helper.run_forever()
+    helper.run_forever(debug=True)
